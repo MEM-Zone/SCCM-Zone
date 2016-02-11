c37f0860510707cff6d3206384a3995ac0359a8c
Ioan Popovici <me@ioan-popovici.ro>
2016-02-11 19:19:51 +0200

    Cleanup

---
+++ Powershell/Clean-CCMCache/Clean-CCMCache.ps1
@@ -0,0 +1,327 @@
+<#
+*********************************************************************************************************
+*                                                                                                       *
+*** This Powershell Script is used to clean the CCM cache of all non persisted content                ***
+*                                                                                                       *
+*********************************************************************************************************
+* Created by Ioan Popovici, 13/11/2015  | Requirements Powershell 3.0                                   *
+* ======================================================================================================*
+* Modified by   |    Date    | Revision |                            Comments                           *
+*_______________________________________________________________________________________________________*
+* Ioan Popovici | 13/11/2015 | v1.0     | First version                                                 *
+* Ioan Popovici | 16/11/2015 | v1.1     | Improved Logging                                              *
+* Ioan Popovici | 16/11/2015 | v1.2     | Vastly Improved                                               *
+* Ioan Popovici | 03/02/2016 | v2.0     | Vastly Improved                                               *
+* Ioan Popovici | 04/02/2016 | v2.1     | Fixed TotalSize decimals                                      *
+*-------------------------------------------------------------------------------------------------------*
+*                                                                                                       *
+*********************************************************************************************************
+
+	.SYNOPSIS
+        	This Powershell Script is used to clean the CCM cache of all non persisted content.
+        .DESCRIPTION
+		This Powershell Script is used to clean the CCM cache of all non persisted content that is not needed anymore.
+	It only cleans packages, applications and updates that have a installed status and are not persisted, othercache items 
+	will NOT be cleaned.
+#>
+
+##*=============================================
+##* INITIALIZATION
+##*=============================================
+#region Initialization
+
+## Cleaning prompt history
+CLS
+
+## Global variable
+$Global:Result  =@()
+
+## Initalize progress Counter
+$ProgressCounter = 0
+
+## Configure Logging
+#  Set log path
+$ResultCSV = "C:\Temp\Clean-CCMCache.log"
+
+#  Remove previous log it it's more than 500 KB
+If (Test-Path $ResultCSV) {
+	If ((Get-Item $ResultCSV).Length -gt 500KB) {
+		Remove-Item $ResultCSV -Force | Out-Null
+	}
+}
+
+#  Get log parent path
+$ResultPath =  Split-Path $ResultCSV -Parent
+
+#  Create path directory if it does not exist
+If ((Test-Path $ResultPath) -eq $False) {
+	New-Item -Path $ResultPath -Type Directory | Out-Null
+}
+
+## Get the current date
+$Date = Get-Date
+
+## Get list of all non persisted content in CCMCache, only this content will be removed
+$CM_CacheItems = Get-WmiObject -Namespace root\ccm\SoftMgmtAgent -Query 'SELECT ContentID,Location FROM CacheInfoEx WHERE PersistInCache = 0'
+
+
+#endregion
+##*=============================================
+##* END INITIALIZATION
+##*=============================================
+
+##*=============================================
+##* FUNCTION LISTINGS
+##*=============================================
+#region FunctionListings
+
+#region Function Remove-CacheItem
+Function Remove-CacheItem {
+<#
+.SYNOPSIS
+	Removes SCCM cache item if it's not persisted.
+.DESCRIPTION
+	Removes specified SCCM cache item if it's not found in the persisted cache list.
+.PARAMETER CacheItemToDelete
+	The cache item ID that needs to be deleted.
+.PARAMETER CacheItemName
+	The cache item name that needs to be deleted.
+.EXAMPLE
+	Remove-CacheItem -CacheItemToDelete "{234234234}" -CacheItemName "Office2003"
+.NOTES
+.LINK
+#>
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory=$true,Position=0)]
+		[Alias('CacheTD')]
+		[string]$CacheItemToDelete,
+		[Parameter(Mandatory=$true,Position=1)]
+		[Alias('CacheN')]
+		[string]$CacheItemName
+	)
+
+	## Delete chache item if it's non persisted
+	If ($CM_CacheItems.ContentID -contains $CacheItemToDelete) {
+
+		#  Get Cache item location and size
+		$CacheItemLocation = $CM_CacheItems | Where {$_.ContentID -Contains $CacheItemToDelete} | Select -ExpandProperty Location
+		$CacheItemSize =  Get-ChildItem $CacheItemLocation -Recurse -Force | Measure-Object -Property Length -Sum | Select -ExpandProperty Sum
+
+		#  Build result object
+		$ResultProps = [ordered]@{
+			'DeletionDate'	= $Date
+			'Name' = $CacheItemName
+			'ID' = $CacheItemToDelete
+			'Location' = $CacheItemLocation
+			'Size(MB)' = "{0:N2}" -f ($CacheItemSize / 1MB)
+			'TotalDeleted(MB)' = ''
+		}
+
+		#  Add items to result object
+		$Global:Result  += New-Object PSObject -Property $ResultProps
+
+		#  Connect to resource manager COM object
+		$CMObject = New-Object -ComObject "UIResource.UIResourceMgr"
+
+	 	#  Use GetCacheInfo method to return cache properties
+		$CMCacheObjects = $CMObject.GetCacheInfo()
+
+		#  Delete Cache element
+		$CMCacheObjects.GetCacheElements() | Where-Object {$_.ContentID -eq $CacheItemToDelete} |
+			ForEach-Object {
+				$CMCacheObjects.DeleteCacheElement($_.CacheElementID)
+			}
+	}
+}
+#endregion
+
+#region Function Remove-CachedApplications
+Function Remove-CachedApplications {
+<#
+.SYNOPSIS
+	Removes SCCM cached application.
+.DESCRIPTION
+	Removes specified SCCM cache application if it's already installed.
+.PARAMETER
+.EXAMPLE
+.NOTES
+.LINK
+#>
+
+	## Get list of applications
+	$CM_Applications = Get-WmiObject -Namespace root\ccm\ClientSDK -Query 'SELECT * FROM CCM_Application'
+
+	## Check for installed applications
+	Foreach ($Application in $CM_Applications) {
+
+		## Show progrss bar
+		If ($CM_Applications.Count -ne $null) {
+			$ProgressCounter++
+			Write-Progress -Activity 'Processing Applications' -CurrentOperation $Application.FullName -PercentComplete (($ProgressCounter / $CM_Applications.Count) * 100)
+			Start-Sleep -Milliseconds 400
+		}
+		## Get Application Properties
+		$Application.Get()
+
+	    ## Enumerate all deployment types for an application
+	    Foreach ($DeploymentType in $Application.AppDTs) {
+	        If ($Application.InstallState -eq "Installed" -and $Application.IsMachineTarget) {
+
+				## Get content ID for specific application deployment type
+	            $AppType = "Install",$DeploymentType.Id,$DeploymentType.Revision
+	            $Content = Invoke-WmiMethod -Namespace root\ccm\cimodels -Class CCM_AppDeliveryType -Name GetContentInfo -ArgumentList $AppType
+
+				## Call Remove-CacheItem function
+				Remove-CacheItem -CacheTD $Content.ContentID -CacheN $Application.FullName
+	        }
+	    }
+	}
+}
+#endregion
+
+#region Function Remove-CachedPackages
+Function Remove-CachedPackages {
+<#
+.SYNOPSIS
+	Removes SCCM cached package.
+.DESCRIPTION
+	Removes specified SCCM cached package if it's not needed anymore.
+.PARAMETER
+.EXAMPLE
+.NOTES
+.LINK
+#>
+
+	## Get list of packages
+	$CM_Packages = Get-WmiObject -Namespace root\ccm\ClientSDK -Query 'SELECT PackageID,PackageName,LastRunStatus,RepeatRunBehavior FROM CCM_Program'
+
+	## Check if any deployed programs in the package need the cached package and add deletion or exemption list for comparison
+	ForEach ($Program in $CM_Packages) {
+
+		#  Check if program in the package needs the cached package
+		If ($Program.LastRunStatus -eq "Succeeded" -and $Program.RepeatRunBehavior -ne "RerunAlways" -and $Program.RepeatRunBehavior -ne "RerunIfSuccess") {
+
+			#  Add PackageID to Deletion List if not already added
+			If ($Program.PackageID -NotIn $PackageIDDeleteTrue) {
+				[array]$PackageIDDeleteTrue += $Program.PackageID
+			}
+
+		} Else {
+
+				#  Add PackageID to Exception List if not already added
+				If ($Program.PackageID -NotIn $PackageIDDeleteFalse) {
+				[array]$PackageIDDeleteFalse += $Program.PackageID
+			}
+		}
+	}
+
+	## Parse Deletion List and Remove Package if not in Exemption List
+	ForEach ($Package in $PackageIDDeleteTrue) {
+
+		#  Show progress bar
+		If ($CM_Packages.Count -ne $null) {
+			$ProgressCounter++
+			Write-Progress -Activity 'Processing Packages' -CurrentOperation $Package.PackageName -PercentComplete (($ProgressCounter / $CM_Packages.Count) * 100)
+			Start-Sleep -Milliseconds 800
+		}
+		#  Call Remove Function if Package is not in $PackageIDDeleteFalse
+		If ($Package -NotIn $PackageIDDeleteFalse) {
+			Remove-CacheItem -CacheTD $Package.PackageID -CacheN $Package.PackageName
+		}
+	}
+}
+#endregion
+
+#region Function Remove-CachedUpdates
+Function Remove-CachedUpdates {
+<#
+.SYNOPSIS
+	Removes SCCM cached updates.
+.DESCRIPTION
+	Removes specified SCCM cached update if it's not needed anymore.
+.PARAMETER
+.EXAMPLE
+.NOTES
+.LINK
+#>
+
+	## Get list of updates
+	$CM_Updates = Get-WmiObject -Namespace root\ccm\SoftwareUpdates\UpdatesStore -Query 'SELECT UniqueID,Title,Status FROM CCM_UpdateStatus'
+
+	## Check if cached updates are not needed and delete them
+	ForEach ($Update in $CM_Updates) {
+
+		#  Show Progrss bar
+		If ($CM_Updates.Count -ne $null) {
+			$ProgressCounter++
+			Write-Progress -Activity 'Processing Updates' -CurrentOperation $Update.Title -PercentComplete (($ProgressCounter / $CM_Updates.Count) * 100)
+		}
+
+		#  Check if update is already installed
+		If ($Update.Status -eq "Installed") {
+
+			#  Call Remove-CacheItem function
+			Remove-CacheItem -CacheTD $Update.UniqueID -CacheN $Update.Title
+		}
+	}
+}
+#endregion
+
+#endregion
+##*=============================================
+##* END FUNCTION LISTINGS
+##*=============================================
+
+##*=============================================
+##* SCRIPT BODY
+##*=============================================
+#region ScriptBody
+
+## Call Remove-CachedApplications function
+Remove-CachedApplications
+
+## Call Remove-CachedApplications function
+Remove-CachedPackages
+
+## Call Remove-CachedApplications function
+Remove-CachedUpdates
+
+## Get Result sort it and build Result Object
+$Result =  $Global:Result | Sort-Object Size`(MB`) -Descending
+
+#  Calculate total deleted size
+$TotalDeletedSize = $Result | Measure-Object -Property Size`(MB`) -Sum | Select -ExpandProperty Sum
+
+#  If $TotalDeletedSize is zero write that nothing could be deleted
+If ($TotalDeletedSize -eq $null) {
+	$TotalDeletedSize = "Nothing to Delete!"
+}
+
+#  Build Result Object
+$ResultProps = [ordered]@{
+	'DeletionDate' = $Date
+	'Name' = 'Total Size of Items Deleted in MB:'
+	'ID' = ''
+	'Location' = ''
+	'Size(MB)' = ''
+	'TotalDeleted(MB)' = '{0:N2}' -f $TotalDeletedSize
+}
+
+#  Add total items deleted to result object
+$Result += New-Object PSObject -Property $ResultProps
+
+## Write Result Object to csv file (append)
+$Result | Export-Csv -Path $ResultCSV -Delimiter ";" -Encoding UTF8 -NoTypeInformation -Append -Force
+
+## Write Result to console
+$Result | Format-Table Name,TotalDeleted`(MB`)
+
+## Let the user know we are finished
+Write-Host ""
+Write-Host "Processing Finished!" -BackgroundColor Green -ForegroundColor White
+
+#endregion
+##*=============================================
+##* END SCRIPT BODY
+##*=============================================
---
+++ Powershell/Set-MaintenanceWindows/Set-MaintenanceWindows.ps1
@@ -0,0 +1,332 @@
+<#
+*********************************************************************************************************
+*                                                                                                       *
+*** This Powershell Script is used to set Maintenance Windows based on PatchTuesday on SU Collections ***
+*                                                                                                       *
+*********************************************************************************************************
+* Created by Ioan Popovici, 30/03/2015  | Requirements Powershell 2.0					*
+* ======================================================================================================*
+* Modified by			|    Date    |	Revision  | 			Comments		*
+*_______________________________________________________________________________________________________*
+* Ioan Popovici/Octavian Cordos | 30/03/2015 | v1.0	  | First version				*
+* Ioan Popovici/Octavian Cordos | 31/03/2015 | v2.0  	  | Vastly Improved				*
+* Ioan Popovici			| 08/01/2016 | v2.1	  | Fixed Locale				*
+* Octavian Cordos               | 11/01/2016 | v2.2	  | Improved MW Naming				*
+* Ioan Popovici			| 11/01/2016 | v2.3	  | Added Logging and Error Detection, cleanup	*
+*-------------------------------------------------------------------------------------------------------*
+*                                                                                                       *
+*********************************************************************************************************
+
+	.SYNOPSIS
+        	This Powershell Script is used to set Maintenance Windows based on PatchTuesday on SU Collections.
+    	.DESCRIPTION
+        	This Powershell Script is used to set Maintenance Windows based on PatchTuesday on SU Collections.
+#>
+
+##*=============================================
+##* INITIALIZATION
+##*=============================================
+#region Initialization
+
+## Cleaning prompt history
+CLS
+
+## Get Running Path
+$CurrentDir = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
+
+## Import Modules and Files
+$MWCSVData = Import-Csv $CurrentDir\"Set-MaintenanceWindows.csv"
+Import-Module "E:\SCCM\AdminConsole\bin\ConfigurationManager.psd1"
+
+## Set Global variables
+$MonthArray = New-Object System.Globalization.DateTimeFormatInfo
+$MonthNames = $MonthArray.MonthNames
+
+## Log Initialization
+$LogPath = "C:\Temp\MW"
+$ErrorLog = $LogPath+"\MWError.log"
+If ((Test-Path $LogPath) -eq $False) {
+	New-Item -Path $LogPath -Type Directory | Out-Null
+} ElseIf (Test-Path $LogPath) {
+		Remove-Item $LogPath\* -Recurse -Force
+	}
+#  Clean Log
+Get-Date | Out-File $ErrorLog -Force
+
+## Change Path to CM Site
+CD VSM:
+
+#endregion
+##*=============================================
+##* END INITIALIZATION
+##*=============================================
+
+##*=============================================
+##* FUNCTION LISTINGS
+##*=============================================
+#region FunctionListings
+
+#region Function Write-Log
+Function Write-Log {
+<#
+.SYNOPSIS
+	Write messages to a log file in CMTrace.exe compatible format or Legacy text file format.
+.DESCRIPTION
+	Write messages to a log file in CMTrace.exe compatible format or Legacy text file format and optionally display in the console.
+.PARAMETER Message
+	The message to write to the log file or output to the console.
+.EXAMPLE
+	Write-Log -Message "Error"
+.NOTES
+.LINK
+#>
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory=$true,Position=0)]
+		[Alias('Text')]
+		[string]$Message
+	)
+
+	## Getting the Date and time
+	$DateAndTime = Get-Date
+
+	### Writing to log file
+	"$DateAndTime : $Message" | Out-File $ErrorLog -Append
+	"$DateAndTime : $_" | Out-File $ErrorLog -Append
+
+	## Writing to Console
+	Write-Host $Message  -ForegroundColor Red -BackgroundColor White
+	Write-Host $_.Exception -ForegroundColor White -BackgroundColor Red
+	Write-Host ""
+}
+#endregion
+
+#region Function Get-PatchTuesday
+Function Get-PatchTuesday {
+<#
+.SYNOPSIS
+	Calculate Microsoft Patch Tuesday and return it to the pipeline.
+.DESCRIPTION
+	Get Microsoft Patch tuesday for a specific month.
+.PARAMETER Year
+	The year for which to calculate Patch Tuesday.
+.PARAMETER Month
+	The month for which to calculate Patch Tuesday.
+.EXAMPLE
+	Get-PatchTuesday -Year 2015 -Month 3
+.NOTES
+.LINK
+#>
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory=$true,Position=0)]
+		[Alias('Yr')]
+		[string]$Year,
+		[Parameter(Mandatory=$true,Position=1)]
+		[Alias('Mo')]
+		[string]$Month
+	)
+
+	## Build Target Month
+	[DateTime]$StartingMonth = $Month+"/1/"+$Year
+
+	## Search for First Tuesday
+    While ($StartingMonth.DayofWeek -ine "Tuesday") {
+		$StartingMonth = $StartingMonth.AddDays(1)
+	}
+
+	## Set Second Tuesday of the month by adding 7 days
+    $PatchTuesday = $StartingMonth.AddDays(7)
+
+	## Return Patch Tuesday
+	Return $PatchTuesday
+ }
+#endregion
+
+#region Function Remove-MaintenanceWindows
+Function Remove-MaintenanceWindows {
+<#
+.SYNOPSIS
+	Remove Existing Maintenance Windows.
+.DESCRIPTION
+	Remove existing maintenance windows from a collection.
+.PARAMETER CollectionName
+	The collection name for which to remove the Mainteance Windows.
+.EXAMPLE
+	Remove-MaintenanceWindows -Collection "Computer Collection"
+.NOTES
+.LINK
+#>
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory=$true,Position=0)]
+		[Alias('Collection')]
+		[string]$CollectionName
+	)
+
+	## Get CollectionID
+	Try {
+		$CollectionID = (Get-CMDeviceCollection -Name $CollectionName -ErrorAction Stop -ErrorVariable Error).CollectionID
+	}
+	Catch {
+		Write-Log -Message "Getting $CollectionName ID Failed!"
+	}
+
+	## Get Collection Maintenance Windows and delete them
+	Try {
+		Get-CMMaintenanceWindow -CollectionId $CollectionID | ForEach-Object {
+			Remove-CMMaintenanceWindow -CollectionID $CollectionID -Name $_.Name -Force -ErrorAction Stop -ErrorVariable Error
+			Write-Host $_.Name " - Removed!"  -ForegroundColor Green
+		}
+	}
+	Catch {
+
+		#  Write to log in case of failure
+		Write-Log -Message "$_.Name  - Removal Failed!"
+	}
+}
+#endregion
+
+#region Function Set-MaintenanceWindows
+Function Set-MaintenanceWindows {
+<#
+.SYNOPSIS
+	Remove Existing Maintenance Windows.
+.DESCRIPTION
+	Remove existing maintenance windows from a collection.
+.PARAMETER CollectionName
+	The collection name for which to remove the Mainteance Windows.
+.PARAMETER MWYear
+	The Maintenance Window Start Year.
+.PARAMETER MWMonth
+	The Maintenance Window Start Month.
+.PARAMETER MWOffsetWeeks
+	The Maintenance Window offset number of weeks after Patch Tuesday.
+.PARAMETER MWOffsetWeeks
+	The Maintenance Window offset number of days after Tuesday.
+.PARAMETER MWStartTime
+	The Maintenance Window Start Time.
+.PARAMETER MWStopTime
+	The Maintenance Window Stop Time.
+.EXAMPLE
+	Remove-MaintenanceWindows -CollectionName "Computer Collection" -MWYear 2015 -MWMonth 3 -MWOffsetWeeks 3 -MWOffsetDays 2 -MWStartTime "01:00:00"  -MWStopTime "02:00:00"
+.NOTES
+.LINK
+#>
+	Param (
+		[Parameter(Mandatory=$true,Position=0)]
+		[Alias('Collection')]
+		[string]$CollectionName,
+		[Parameter(Mandatory=$true,Position=1)]
+		[Alias('Year')]
+		[int16]$MWYear,
+		[Parameter(Mandatory=$true,Position=2)]
+		[Alias('Month')]
+		[int16]$MWMonth,
+		[Parameter(Mandatory=$true,Position=3)]
+		[Alias('Weeks')]
+		[int16]$MWOffsetWeeks,
+		[Parameter(Mandatory=$true,Position=4)]
+		[Alias('Days')]
+		[int16]$MWOffsetDays,
+		[Parameter(Mandatory=$true,Position=5)]
+		[Alias('StartTime')]
+		[string]$MWStartTime,
+		[Parameter(Mandatory=$true,Position=6)]
+		[Alias('StopTime')]
+		[string]$MWStopTime
+	)
+
+	## Get CollectionID
+	Try {
+		$CollectionID = (Get-CMDeviceCollection -Name $CollectionName -ErrorAction Stop -ErrorVariable Error).CollectionID
+	}
+	Catch {
+
+		#  Write to log in case of failure
+		Write-Log "Getting $CollectionName ID - Failed!"
+	}
+
+	## Get PatchTuesday
+	$PatchTuesday = Get-PatchTuesday $MWYear $MWMonth
+
+	## Setting Patch Day, adding offset days and weeks. Get-Date is used to get the date in the same cast, otherwise we cannot convert the date from string to datetime format.
+	$PatchDay = (Get-Date -Date $PatchTuesday).AddDays($MWOffsetDays+($MWOffsetWeeks*7))
+
+	## Check if we got ourselves in the next year and return to the main script if true
+	If ($PatchDay.Year -gt $MWYear) {
+		Write-Warning "Year threshold detected! Ending Cycle..."
+		Return
+	}
+
+	## Setting Maintenance Window Start and Stop times
+	$MWStartTime = (Get-Date -Format "M/dd/yyyy HH:mm:ss" -Date $PatchDay) -Replace "00:00:00", $MWStartTime
+	$MWStopTime = (Get-Date -Format "M/dd/yyyy HH:mm:ss" -Date $PatchDay) -Replace "00:00:00", $MWStopTime
+
+	## Create The Schedule Token
+    $MWSchedule = New-CMSchedule -Start $MWStartTime -End $MWStopTime -NonRecurring
+
+    ## Set Maintenance Window Naming Convention MW Month and Actual Patches being deployed Month
+    $MWName =  'MW.NR.'+(Get-Date -Uformat %B $MWStartTime)+'.Patch_Group_'+$MWOffsetWeeks+'.'+$MonthNames[$MWMonth-1]+'_Updates'
+
+	## Set Maintenance Window
+    Try {
+		$SetNewMW = New-CMMaintenanceWindow -CollectionID $CollectionID -Schedule $MWSchedule -Name $MWName -ApplyTo Any -ErrorVariable Error -ErrorAction Stop
+		Write-Host "Setting $MWName on $CollectionName - Successful" -ForegroundColor Green
+	}
+	Catch {
+
+		#  Write to log in case of failure
+		Write-Log "Setting $MWName on $CollectionName - Failed!"
+	}
+}
+#endregion
+
+#endregion
+##*=============================================
+##* END FUNCTION LISTINGS
+##*=============================================
+
+##*=============================================
+##* SCRIPT BODY
+##*=============================================
+#region ScriptBody
+
+## Process Imported CSV Object Data
+$MWCSVData | ForEach-Object {
+
+	#  Check if we need the Remove Existing Maintenance Window is set
+	If ($_.RemoveExistingMW -eq 1) {
+		Write-Host ""
+		Write-Host "Remove Existing Maintenance Windows from" $_.CollectionName":" -ForegroundColor Blue -BackgroundColor White
+		Write-Host ""
+		Remove-MaintenanceWindows $_.CollectionName
+	}
+
+	#  Check if we need to set Maintenance Windows for the whole year
+	If ($_.SetForWholeYear -eq 1) {
+		Write-Host ""
+		Write-Host "Setting Maintenance Windows on" $_.CollectionName "for the whole year:" -ForegroundColor Blue -BackgroundColor White
+		Write-Host ""
+		For ($Month = [int]$_.MWMonth; $Month -le 12; $Month++) {
+			Set-MaintenanceWindows -CollectionName $_.CollectionName -MWYear $_.MWYear -MWMonth $Month -MWOffsetWeeks $_.MWOffsetWeeks -MWOffsetDays $_.MWOffsetDays -MWStartTime $_.MWStartTime -MWStopTime $_.MWStopTime
+		}
+
+	#  Run without removing Maintenance Windows and set Maintenance Window just for one month
+	} Else {
+			Write-Host ""
+			Write-Host "Setting Maintenance Windows on" $_.CollectionName "for " $MonthNames[$_.MWMonth-1]":" -ForegroundColor Blue -BackgroundColor White
+			Write-Host ""
+			Set-MaintenanceWindows -CollectionName $_.CollectionName -MWYear $_.MWYear -MWMonth $_.MWMonth -MWOffsetWeeks $_.MWOffsetWeeks -MWOffsetDays $_.MWOffsetDays -MWStartTime $_.MWStartTime -MWStopTime $_.MWStopTime
+		}
+}
+Write-Host ""
+Write-Host "Processing Finished!" -BackgroundColor Green -ForegroundColor White
+
+## Return to Script Path
+CD $CurrentDir
+
+#endregion
+##*=============================================
+##* END SCRIPT BODY
+##*=============================================
