3f9a1f23ed655d0f65854ed68d2c9f231cab57c3
Ioan Popovici <me@ioan-popovici.ro>
2017-09-22 18:16:05 +0200

    Reporting
    Added Baseline Compliance Report
    Modifications to Bitlocker Script

---
+++ Powershell/Get-BitlockerStatus/Get-BitlockerStatus.ps1
@@ -0,0 +1,142 @@
+<#
+*********************************************************************************************************
+* Created by Ioan Popovici   | Requires PowerShell 3.0                                                  *
+* ===================================================================================================== *
+* Modified by   |    Date    | Revision | Comments                                                      *
+* _____________________________________________________________________________________________________ *
+* Ioan Popovici | 2015-11-15 | v1.0     | First version                                                 *
+* Ioan Popovici | 2017-09-22 | v1.1     | Modified for all drives, improvements and code cleanup        *
+* ===================================================================================================== *
+*                                                                                                       *
+*********************************************************************************************************
+
+.SYNOPSIS
+    This PowerShell Script is used to get the BitLocker protection status.
+.DESCRIPTION
+    This PowerShell Script is used to get the BitLocker protection status for a specific drive, or all drives.
+.EXAMPLE
+    C:\Windows\System32\WindowsPowerShell\v1.0\PowerShell.exe -NoExit -NoProfile -File Get-BitLockerStatus.ps1
+.LINK
+    https://sccm-zone.com
+    https://github.com/JhonnyTerminus/SCCM
+#>
+
+##*=============================================
+##* VARIABLE DECLARATION
+##*=============================================
+#region VariableDeclaration
+
+## Initializing Result Object
+[psCustomObject]$Result =@()
+[array]$ResultProps =@()
+
+## Initializing variables
+[string]$LocalDrives
+
+#endregion
+##*=============================================
+##* END VARIABLE DECLARATION
+##*=============================================
+
+##*=============================================
+##* FUNCTION LISTINGS
+##*=============================================
+#region FunctionListings
+
+#region Function Get-BitLockerStatus
+Function Get-BitLockerStatus {
+<#
+.SYNOPSIS
+    This Function is used the get the BitLocker Protection Status.
+.DESCRIPTION
+    This Function is used the get the BitLocker Protection Status.
+.PARAMETER DriveLetter
+    Drive Letter to check for BitLocker Status. Optional Parameter.
+.EXAMPLE
+    Get-BitLockerStatus -DriveLetter 'D'
+.NOTES
+    This is an internal script function and should typically not be called directly.
+.LINK
+    https://sccm-zone.com
+    https://github.com/JhonnyTerminus/SCCM
+#>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$false,Position=0)]
+        [Alias('Drive')]
+        [string]$DriveLetter
+    )
+
+    Try {
+
+        ##  Get the local drives from WMI
+        $LocalDrives = Get-CimInstance -Namespace 'root\CIMV2' -ClassName 'CIM_LogicalDisk' | Where-Object { $_.DriveType -eq '3' }
+
+        ## Get the BitLocker Status for all drives from WMI
+        Get-CimInstance  -Namespace 'root\CIMV2\Security\MicrosoftVolumeEncryption'  -ClassName 'Win32_EncryptableVolume' -ErrorAction 'Stop' | ForEach-Object {
+
+            #  Create the Result Props and make the ProtectionStatus more report friendly
+            $ResultProps = [ordered]@{
+                'Drive' = $_.DriveLetter
+                'ProtectionStatus' = $(
+                    Switch ($_.ProtectionStatus) {
+                        0 { 'PROTECTION OFF' }
+                        1 { 'PROTECTION ON' }
+                        2 { 'PROTECTION UNKNOWN' }
+                    }
+                )
+            }
+
+            #  Adding ResultProps hash table to result object
+            $Result += New-Object PSObject -Property $ResultProps
+        }
+
+        #  Workaround for some Windows 7 computers not reporting BitLocker protection status for all drives
+        #  Create the ResultProps array
+        $LocalDrives | ForEach-Object {
+            If ($_.DeviceID -notin $Result.Drive) {
+                $ResultProps = [ordered]@{
+                    'Drive' = $_.DeviceID
+                    'ProtectionStatus' = 'PROTECTION OFF'
+                }
+
+                #  Adding ResultProps hash table to result object
+                $Result += New-Object PSObject -Property $ResultProps
+            }
+        }
+    }
+
+    ## Catch any script errors
+    Catch {
+        Write-Host "Script Execution - Error!`n $_`n" -ForegroundColor 'Red' -BackgroundColor 'Black'
+    }
+    Finally {
+
+        ## Return different Results depending on wether the DriveLetter parameter was specified or not
+        If ($DriveLetter) {
+            Write-Output -InputObject $($Result | Where-Object { $_.Drive -match $DriveLetter })
+        }
+        Else {
+            Write-Output -InputObject $Result
+        }
+    }
+}
+#endregion
+
+#endregion
+##*=============================================
+##* END FUNCTION LISTINGS
+##*=============================================
+
+##*=============================================
+##* SCRIPT BODY
+##*=============================================
+#region ScriptBody
+
+## Write BitLockerStatus to console
+Write-Host "$(Get-BitLockerStatus | Format-Table | Out-String)" -ForegroundColor 'Yellow' -BackgroundColor 'Black'
+
+#endregion
+##*=============================================
+##* END SCRIPT BODY
+##*=============================================
