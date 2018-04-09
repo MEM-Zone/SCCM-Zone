ad7accaecc5d9437a0bd657a177c44c48a50dfe0
Ioan Popovici <me@ioan-popovici.ro>
2018-04-09 17:18:02 +0200

    Code reformating
    Code reformating for more visibility

--- Reporting/CSM-Compliance-Configuration_Baseline/CSM-Compliance-Configuration_Baseline.sql
+++ Reporting/CSM-Compliance-Configuration_Baseline/CSM-Compliance-Configuration_Baseline.sql
@@ -5,6 +5,7 @@
 * Modified by   |    Date    | Revision | Comments                                                      *
 * _____________________________________________________________________________________________________ *
 * Ioan Popovici | 2017-09-22 | v1.0     | First version                                                 *
+* Ioan Popovici | 2018-01-17 | v1.1     | Fixed: Actual Value is NULL compliance is not displayed       *
 * ===================================================================================================== *
 *                                                                                                       *
 *********************************************************************************************************
@@ -25,40 +26,39 @@
 /*##=============================================*/
 /* #region QueryBody */
 
-SELECT Distinct
-    SYS.Name0 AS 'Computer Name',
-    SNames.StateName AS 'Compliance State',
-    CCS.LastComplianceMessageTime as 'Last Compliance Evaluation',
-    SYS.User_Name0 as 'User Name',
-    OS.Caption0 as 'Operating System',
-    OS.InstallDate0 as 'Install Date',
-    STATUS.LastHWScan as 'Last HW Scan',
-    COMP.Model0 as 'Model',
-    CI.CIVersion AS 'Baseline Content Version',
-    CSCSD.CurrentValue,
+SELECT DISTINCT
+    Computer.Name0 AS 'Computer Name',
+    StateName.StateName AS 'Compliance State',
+    ComplianceStatus.LastComplianceMessageTime as 'Last Compliance Evaluation',
+    Users.User_Name0 AS 'User Name',
+	OperatingSystem.Caption0 AS 'Operating System',
+	OperatingSystem.InstallDate0 AS 'Install Date',
+    ComputerStatus.LastHWScan AS 'Last HW Scan',
+	Computer.Model0 AS 'Model',
+    ConfigurationItem.CIVersion AS 'Baseline Content Version',
+    CurrentComplianceDetails.CurrentValue,
 
     /* IMPORTANT! YOU NEED TO ENABLE THE COMPANY FIELD GATHERING FOR SYSTEM DISCOVERY OTHERWISE THIS COLUMN IS NOT AVAILABLE */
-    SYS.Company0 as 'Company'
-    --( SELECT [CM_Tools].[dbo].[ufn_GetCompany_by_ResourceID]([sys].[ResourceID]) ) AS [Company]
+    Users.Company0 AS 'Company'
+    --( SELECT [CM_Tools].[dbo].[ufn_GetCompany_by_ResourceID]([Users].[ResourceID]) ) AS [Company]
 FROM
-    v_BaselineTargetedComputers BTC
-    INNER JOIN fn_rbac_R_System(@UserSIDs) as SYS ON SYS.ResourceID = BTC.ResourceID
-    LEFT OUTER JOIN v_ClientCollectionMembers c ON C.ResourceID = SYS.ResourceID
-    INNER JOIN v_GS_COMPUTER_SYSTEM COMP on COMP.ResourceID = BTC.ResourceID
-    INNER JOIN v_GS_OPERATING_SYSTEM OS on OS.ResourceID = SYS.ResourceID
-    INNER JOIN v_ConfigurationItems CI ON CI.CI_ID = BTC.CI_ID
-    INNER JOIN v_CICurrentComplianceStatus CCS ON CCS.CI_ID = CI.CI_ID AND CCS.ResourceID = BTC.ResourceID
-    INNER JOIN v_CIComplianceStatusComplianceDetail CSCD ON CSCD.CI_ID = CI.CI_ID
-    INNER JOIN v_CICurrentSettingsComplianceStatusDetail CSCSD ON CSCSD.CI_ID = CSCD.Setting_CI_ID
-    INNER JOIN v_LocalizedCIProperties_SiteLoc CIProp ON CIProp.CI_ID = CI.CI_ID
-    INNER JOIN v_StateNames SNames ON CCS.ComplianceState = SNames.StateID
-    LEFT OUTER JOIN v_GS_WORKSTATION_STATUS STATUS on STATUS.ResourceID=SYS.ResourceID
-    LEFT OUTER JOIN v_R_User USR on USR.User_Name0 = SYS.User_Name0
+    v_BaselineTargetedComputers Baseline
+	INNER JOIN fn_rbac_R_System(@UserSIDs) AS Users ON Users.ResourceID = Baseline.ResourceID
+    LEFT OUTER JOIN v_ClientCollectionMembers AS Collections ON Collections.ResourceID = Users.ResourceID
+    INNER JOIN v_GS_COMPUTER_SYSTEM Computer ON Computer.ResourceID = Baseline.ResourceID
+	INNER JOIN v_GS_OPERATING_SYSTEM AS OperatingSystem ON OperatingSystem.ResourceID = Users.ResourceID
+    INNER JOIN v_ConfigurationItems AS ConfigurationItem ON ConfigurationItem.CI_ID = Baseline.CI_ID
+    INNER JOIN v_CICurrentComplianceStatus AS ComplianceStatus ON ComplianceStatus.CI_ID = ConfigurationItem.CI_ID AND ComplianceStatus.ResourceID = Baseline.ResourceID
+	INNER JOIN v_CIComplianceStatusComplianceDetail AS ComplianceDetails ON ComplianceDetails.CI_ID = ConfigurationItem.CI_ID
+	LEFT  JOIN v_CICurrentSettingsComplianceStatusDetail AS CurrentComplianceDetails ON CurrentComplianceDetails.CI_ID = ComplianceDetails.Setting_CI_ID
+    INNER JOIN v_LocalizedCIProperties_SiteLoc AS BaselineProperties ON BaselineProperties.CI_ID = ConfigurationItem.CI_ID
+    INNER JOIN v_StateNames AS StateName ON ComplianceStatus.ComplianceState = StateName.StateID
+    LEFT OUTER JOIN v_GS_WORKSTATION_STATUS AS ComputerStatus ON ComputerStatus.ResourceID = Users.ResourceID
 WHERE
-    CIProp.DisplayName = @Baseline
-        AND SNames.TopicType = 401
-        AND C.CollectionID= @CollID
-ORDER BY SNames.StateName
+    BaselineProperties.DisplayName = @BaselineName
+        AND StateName.TopicType = 401
+        AND Collections.CollectionID= @CollectionID
+ORDER BY StateName.StateName
 
 /* #endregion */
 /*##=============================================*/
