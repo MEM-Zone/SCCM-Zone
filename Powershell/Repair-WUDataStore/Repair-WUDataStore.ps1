3dc29194fbb049928807f20808045ed35ea1d324
Ioan Popovici <me@ioan-popovici.ro>
2018-05-17 15:34:26 +0200

    Generalized for multiple error use

--- Powershell/Repair-WUDataStore/Repair-WUDataStore.ps1
+++ Powershell/Repair-WUDataStore/Repair-WUDataStore.ps1
@@ -6,6 +6,7 @@
 * _____________________________________________________________________________________________________ *
 * Ioan Popovici     | 2018-03-28 | v1.0     | First version                                             *
 * Ioan Popovici     | 2018-05-16 | v1.1     | Fixed logical bugs that forced a NULL return              *
+* Ioan Popovici     | 2018-05-17 | v1.2     | Generalized so it can be used for multiple error cases    *
 * ===================================================================================================== *
 *                                                                                                       *
 *********************************************************************************************************
@@ -14,21 +15,33 @@
     Repairs a corrupted WU DataStore.
 .DESCRIPTION
     Detects and repairs a corrupted WU DataStore.
-    Detection is done by counting ESENT errors from the last 3 days in the Application eventlog with the EventID equal to '623'.
+    Detection is done by testing the eventlog with the specified parameters.
     Repairs are performed by removing and reinitializing the corrupted DataStore.
-    After the repair step completes the Application eventlog is backed up and cleared in order not to triger the detection again.
-    The backup of the Application log is stored in 'SystemRoot\Temp' folder.
+    The specified eventlog is backed up and cleared in order not to triger the detection again before the repair step.
+    The backup of the specified eventlog is stored in 'SystemRoot\Temp' folder.
+    Defaults are configured for the ESENT '623' error.
 .PARAMETER Action
     Specifies the action to be performed. Available actions are: ('DetectAndRepair', 'Detect', 'Repair').
-    Default is: 'DetectAndRepair'
+.PARAMETER LogName
+    Specifies the LogName to search. Default is: 'Application'
+.PARAMETER Source
+    Specifies the Source to search. Default is: 'ESENT'
+.PARAMETER EventID
+    Specifies the EventID to search. Default is: '623'
+.PARAMETER EntryType
+    Specifies the Entry Type to search. Available options are: ('Information','Warning','Error'). Default is: 'Error'.
+.PARAMETER LimitDays
+    Specifies the number of days from the current date to limit the search to. Default is: 3.
+.PARAMETER Threshold
+    Specifed the numbers of events after which this functions returns $true. Default is: 3.
 .EXAMPLE
-    Repair-WUDataStore -Action 'Detect'
+    Repair-WUDataStore -Action 'Detect' -LogName 'Application' -Source 'ESENT' -EventID '623' -EntryType 'Error' -LimitDays 3 -Threshold 3
 .INPUTS
     System.String.
 .OUTPUTS
-    None. This function has no outputs.
+    System.String. This script returns Compliant, Non-Compliant, Remediated or Error Message
 .NOTES
-    This function can typically be called directly.
+    This script can be called directly.
 .LINK
     https://sccm-zone.com
 .LINK
@@ -46,19 +59,29 @@
 
 ## Get script parameters
 Param (
-    [Parameter(Mandatory=$false,Position=0)]
+    [Parameter(Mandatory=$true,Position=0)]
     [ValidateNotNullorEmpty()]
     [ValidateSet('DetectAndRepair','Detect','Repair')]
-    [string]$Action = 'DetectAndRepair'
+    [string]$Action,
+    [Parameter(Mandatory=$false,Position=1)]
+    [ValidateNotNullorEmpty()]
+    [string]$LogName = 'Application',
+    [Parameter(Mandatory=$false,Position=2)]
+    [ValidateNotNullorEmpty()]
+    [string]$Source = 'ESENT',
+    [Parameter(Mandatory=$false,Position=3)]
+    [ValidateNotNullorEmpty()]
+    [string]$EventID = '623' ,
+    [Parameter(Mandatory=$false,Position=4)]
+    [ValidateNotNullorEmpty()]
+    [string]$EntryType = 'Error',
+    [Parameter(Mandatory=$false,Position=5)]
+    [ValidateNotNullorEmpty()]
+    [int]$LimitDays = 3,
+    [Parameter(Mandatory=$false,Position=6)]
+    [ValidateNotNullorEmpty()]
+    [int]$Threshold = 3
 )
-
-## Set EventLog variables
-$LogName = 'Application'
-$Source = 'ESENT'
-$EventID = '623'
-$EntryType = 'Error'
-$After = $((Get-Date).AddDays(-3))
-$Threshold = 3
 
 #endregion
 ##*=============================================
@@ -69,6 +92,98 @@ $Threshold = 3
 ##* FUNCTION LISTINGS
 ##*=============================================
 #region FunctionListings
+
+#region Function Test-EventLogCompliance
+Function Test-EventLogCompliance {
+<#
+.SYNOPSIS
+    Tests the EventLog compliance for specific events.
+.DESCRIPTION
+    Tests the EventLog compliance by getting events and returing a Non-Compliant statement after a specified treshold is reached.
+.PARAMETER LogName
+    Specifies the LogName to search.
+.PARAMETER Source
+    Specifies the Source to search.
+.PARAMETER EventID
+    Specifies the EventID to search.
+.PARAMETER EntryType
+    Specifies the Entry Type to search. Available options are: ('Information','Warning','Error'). Default is: 'Error'.
+.PARAMETER LimitDays
+    Specifies the number of days from the current date to limit the search to.
+    Default is: 1.
+.PARAMETER Threshold
+    Specifed the numbers of events after which this functions returns $true.
+.EXAMPLE
+    Test-EventLogCompliance -LogName 'Application' -Source 'ESENT' -EventID '623' -EntryType 'Error' -LimitDays 3 -Threshold 3
+.INPUTS
+    None.
+.OUTPUTS
+    System.Boolean.
+.NOTES
+    This function can typically be called directly.
+.LINK
+    https://sccm-zone.com
+.LINK
+    https://github.com/JhonnyTerminus/SCCM
+.COMPONENT
+    WindowsUpdate
+.FUNCTIONALITY
+    Test
+#>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$true,Position=0)]
+        [ValidateNotNullorEmpty()]
+        [string]$LogName,
+        [Parameter(Mandatory=$true,Position=1)]
+        [ValidateNotNullorEmpty()]
+        [string]$Source,
+        [Parameter(Mandatory=$true,Position=2)]
+        [ValidateNotNullorEmpty()]
+        [string]$EventID,
+        [Parameter(Mandatory=$false,Position=3)]
+        [ValidateSet('Information','Warning','Error')]
+        [string]$EntryType = 'Error',
+        [Parameter(Mandatory=$false,Position=4)]
+        [ValidateNotNullorEmpty()]
+        [int]$LimitDays = 1,
+        [Parameter(Mandatory=$true,Position=5)]
+        [ValidateNotNullorEmpty()]
+        [int]$Threshold
+    )
+
+    Try {
+
+        ## Set day limit by substracting number of days from the current date
+        $After = $((Get-Date).AddDays( - $LimitDays ))
+
+        ## Get events and test treshold
+        $Events = Get-EventLog -ComputerName $env:COMPUTERNAME -LogName $LogName -Source $Source -EntryType $EntryType -After $After -ErrorAction 'Stop' | Where-Object { $_.EventID -eq $EventID }
+
+        If ($Events.Count -ge $Threshold) {
+            $Compliance = 'Non-Compliant'
+        }
+        Else {
+            $Compliance = 'Compliant'
+        }
+    }
+    Catch {
+
+        ## Set result as 'Compliant' if no matches are found
+        If ($($_.Exception.Message) -match 'No matches found') {
+            $Compliance =  'Compliant'
+        }
+        Else {
+            $Compliance = "Eventlog [$EventLog] compliance test error. $($_.Exception.Message)"
+        }
+    }
+    Finally {
+
+        ## Return Compliance result
+        Write-Output -InputObject $Compliance
+    }
+}
+#endregion
 
 #region Function Backup-EventLog
 Function Backup-EventLog {
@@ -116,17 +231,17 @@ Function Backup-EventLog {
     Begin {
 
         ## Setting variables
-        $PowerShellVersion = $PSVersionTable.PSVersion.Major
-        $Date = $(Get-Date -f 'yyyy-MM-dd_HH-mm-ss')
+        [int]$PowerShellVersion = $PSVersionTable.PSVersion.Major
+        [datetime]$Date = $(Get-Date -f 'yyyy-MM-dd_HH-mm-ss')
         #  Setting optional parameters
         If (-not $BackupPath) {
             $BackupPath = $(Join-Path -Path $Env:SystemRoot -ChildPath '\Temp')
         }
         If (-not $BackupFileName) {
-            $BackUpFileName = "{0}_{1}_{2}.evtx" -f $Date, $Env:COMPUTERNAME, $LogName
+            [string]$BackUpFileName = "{0}_{1}_{2}.evtx" -f $Date, $Env:COMPUTERNAME, $LogName
         }
         #  Setting backup arguments
-        $BackupArguments = @{ ArchiveFileName = (Join-Path -Path $BackupPath -ChildPath $BackUpFileName) }
+        [hashtable]$BackupArguments = @{ ArchiveFileName = (Join-Path -Path $BackupPath -ChildPath $BackUpFileName) }
     }
     Process {
         Try {
@@ -174,94 +289,6 @@ Function Backup-EventLog {
 }
 #endregion
 
-#region Function Test-EventLogCompliance
-Function Test-EventLogCompliance {
-<#
-.SYNOPSIS
-    Tests the EventLog compliance for specific events.
-.DESCRIPTION
-    Tests the EventLog compliance by getting events and returing a Non-Compliant statement after a specified treshold is reached.
-.PARAMETER LogName
-    Specifies the LogName to search.
-.PARAMETER Source
-    Specifies the Source to search.
-.PARAMETER EventID
-    Specifies the EventID to search.
-.PARAMETER EntryType
-    Specifies the Entry Type to search. Available options are: ('Information','Warning','Error'). Default is: 'Error'.
-.PARAMETER After
-    Specifies the data and time that this function get events that occur after. Enter a DateTime object, such as the one returned by the Get-Date cmdlet.
-.PARAMETER Threshold
-    Specifed the treshold after which this functions returns $true.
-.EXAMPLE
-    Test-EventLogCompliance -LogName 'Application' -Source 'ESENT' -EventID '623' -EntryType 'Error' -After $((Get-Date).AddDays(-1)) -Threshold 3
-.INPUTS
-    None.
-.OUTPUTS
-    System.Boolean.
-.NOTES
-    This function can typically be called directly.
-.LINK
-    https://sccm-zone.com
-.LINK
-    https://github.com/JhonnyTerminus/SCCM
-.COMPONENT
-    WindowsUpdate
-.FUNCTIONALITY
-    Test
-#>
-    [CmdletBinding()]
-    Param (
-        [Parameter(Mandatory=$true,Position=0)]
-        [ValidateNotNullorEmpty()]
-        [String]$LogName,
-        [Parameter(Mandatory=$true,Position=1)]
-        [ValidateNotNullorEmpty()]
-        [String]$Source,
-        [Parameter(Mandatory=$true,Position=2)]
-        [ValidateNotNullorEmpty()]
-        [String]$EventID,
-        [Parameter(Mandatory=$false,Position=3)]
-        [ValidateSet('Information','Warning','Error')]
-        [String]$EntryType = 'Error',
-        [Parameter(Mandatory=$true,Position=4)]
-        [ValidateNotNullorEmpty()]
-        [DateTime]$After,
-        [Parameter(Mandatory=$true,Position=5)]
-        [ValidateNotNullorEmpty()]
-        [Int16]$Threshold
-    )
-
-    Try {
-
-        ## Get events and test treshold
-        $Events = Get-EventLog -ComputerName $env:COMPUTERNAME -LogName $LogName -Source $Source -EntryType $EntryType -After $After -ErrorAction 'Stop' | Where-Object { $_.EventID -eq $EventID }
-
-        If ($Events.Count -ge $Threshold) {
-            $Compliance = 'Non-Compliant'
-        }
-        Else {
-            $Compliance = 'Compliant'
-        }
-    }
-    Catch {
-
-        ## Set result as 'Compliant' if no matches are found
-        If ($($_.Exception.Message) -match 'No matches found') {
-            $Compliance =  'Compliant'
-        }
-        Else {
-            $Compliance = "Eventlog [$EventLog] compliance test error. $($_.Exception.Message)"
-        }
-    }
-    Finally {
-
-        ## Return Compliance result
-        Write-Output -InputObject $Compliance
-    }
-}
-#endregion
-
 #region Function Repair-WUDataStore
 Function Repair-WUDataStore {
 <#
@@ -290,8 +317,8 @@ Function Repair-WUDataStore {
     Try {
 
         #  Setting Paths
-        $PathRegsvr = (Join-Path -Path $Env:SystemRoot -ChildPath '\System32\Regsvr32.exe')
-        $PathDataStore = (Join-Path -Path $Env:SystemRoot -ChildPath '\SoftwareDistribution\DataStore')
+        [string]$PathRegsvr = (Join-Path -Path $Env:SystemRoot -ChildPath '\System32\Regsvr32.exe')
+        [string]$PathDataStore = (Join-Path -Path $Env:SystemRoot -ChildPath '\SoftwareDistribution\DataStore')
 
         ## Re-register wuauend.dll
         $null = Start-Process -FilePath $PathRegsvr -ArgumentList '/s Wuaueng.dll' -Wait -ErrorAction 'SilentlyContinue'
@@ -301,12 +328,12 @@ Function Repair-WUDataStore {
 
         ## Wait for the windows update service to stop
         #  Setting Loop index to 12 (one minute)
-        $Loop = 1
+        [int]$Loop = 1
         While ($StatusWuaService -ne 'Stopped') {
 
             #  Waiting 5 seconds
             $null = Start-Sleep -Seconds 5
-            $StatusWuaService =  (Get-Service -Name 'wuauserv').Status
+            [string]$StatusWuaService =  (Get-Service -Name 'wuauserv').Status
 
             #  Incrementing loop index
             $Loop++
@@ -321,10 +348,10 @@ Function Repair-WUDataStore {
         $null = Remove-Item -Path $PathDataStore -Recurse -Force -ErrorAction 'Stop' | Out-Null
 
         ## Set result to 'Remediated'
-        $RepairWuDatastore = 'Remediated'
+        [string]$RepairWuDatastore = 'Remediated'
     }
     Catch {
-        $RepairWuDatastore = "WUDataStore repair error [$($_.Exception.Message)]."
+        [string]$RepairWuDatastore = "WUDataStore repair error [$($_.Exception.Message)]."
     }
     Finally {
 
@@ -351,7 +378,7 @@ Switch ($Action) {
     'DetectAndRepair' {
 
         ## Get machine compliance
-        $ESENTError623 = Test-EventLogCompliance -LogName $LogName -Source $Source -EventID $EventID -EntryType $EntryType -After $After -Threshold $Threshold
+        [string]$ESENTError623 = Test-EventLogCompliance -LogName $LogName -Source $Source -EventID $EventID -EntryType $EntryType -LimitDays $LimitDays -Threshold $Threshold
 
         ## Start processing if compliance test returns 'Non-Compliant'
         If ($ESENTError623 -eq 'Non-Compliant') {
@@ -378,7 +405,7 @@ Switch ($Action) {
     'Detect' {
 
         ## Get machine compliance and return it
-        $ESENTError623 = Test-EventLogCompliance -LogName $LogName -Source $Source -EventID $EventID -EntryType $EntryType -After $After -Threshold $Threshold
+        [string]$ESENTError623 = Test-EventLogCompliance -LogName $LogName -Source $Source -EventID $EventID -EntryType $EntryType -LimitDays $LimitDays -Threshold $Threshold
         Write-Output -InputObject $ESENTError623
     }
     'Repair' {
