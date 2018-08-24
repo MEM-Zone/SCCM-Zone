7f07f82de4832ec8db670fd03eb5bd557ab11b45
Ioan Popovici <me@ioan-popovici.ro>
2018-08-24 17:42:19 +0200

    Convert indentation to spaces

--- Reporting/Software/SD Maintenance Windows/SD Maintenance Windows by Collection and Type.sql
+++ Reporting/Software/SD Maintenance Windows/SD Maintenance Windows by Collection and Type.sql
@@ -27,25 +27,25 @@
 /*##=============================================*/
 
 SELECT
-	Collection.Name AS Collection,
-	ServiceWindow.Name,
-	ServiceWindow.Description,
-	CASE ServiceWindow.ServiceWindowType
-		WHEN 1 THEN 'All Deployments'
-		WHEN 2 THEN 'Programs'
-		WHEN 3 THEN 'Reboot Required'
-		WHEN 4 THEN 'Software Updates'
-		WHEN 5 THEN 'Task Sequences'
-		WHEN 6 THEN 'User Defined'
-	END AS Type,
-	ServiceWindow.StartTime,
-	ServiceWindow.Duration,
-	CASE ServiceWindow.IsEnabled
-		WHEN 1 THEN 'Yes'
-		ELSE 'No'
-	END AS Enabled
+    Collection.Name AS Collection,
+    ServiceWindow.Name,
+    ServiceWindow.Description,
+    CASE ServiceWindow.ServiceWindowType
+        WHEN 1 THEN 'All Deployments'
+        WHEN 2 THEN 'Programs'
+        WHEN 3 THEN 'Reboot Required'
+        WHEN 4 THEN 'Software Updates'
+        WHEN 5 THEN 'Task Sequences'
+        WHEN 6 THEN 'User Defined'
+    END AS Type,
+    ServiceWindow.StartTime,
+    ServiceWindow.Duration,
+    CASE ServiceWindow.IsEnabled
+        WHEN 1 THEN 'Yes'
+        ELSE 'No'
+    END AS Enabled
 FROM dbo.fn_rbac_ServiceWindow(@UserSIDs) AS ServiceWindow
-	JOIN v_Collection AS Collection ON Collection.CollectionID = ServiceWindow.CollectionID
+    JOIN v_Collection AS Collection ON Collection.CollectionID = ServiceWindow.CollectionID
 ORDER BY Name
 
 /*##=============================================*/
