b47733930db1ad7e199126db4ee4e1d501f2046b
Ioan Popovici <me@ioan-popovici.ro>
2018-11-30 19:12:30 +0100

    Released Export-CMDBToTelegraf v1.0
    ## 1.0 - 2018-11-30
    
    ### First version
    
        * Exports data from the SCCM database and formats it for telegraf.

---
+++ Grafana/Cmdlets/Get-CMDBClientInfo.ps1
@@ -0,0 +1,79 @@
+#region Function Get-CMDBClientInfo
+Function Get-CMDBClientInfo {
+<#
+.SYNOPSIS
+    Gets client information.
+.DESCRIPTION
+    Gets client information from the SCCM database.
+.PARAMETER Server
+    Specifies the server name.
+.PARAMETER Database
+    Specifies the database name.
+.EXAMPLE
+    Get-CMDBClientInfo -Server 'SomeServer' -Database 'CM_SomeSiteCode'
+.NOTES
+    This is an internal script function and should typically not be called directly.
+.LINK
+    https://SCCM.Zone
+.LINK
+    https://SCCM.Zone/Git
+#>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [ValidateNotNullorEmpty()]
+        [Alias('Srv')]
+        [string]$Server,
+        [Parameter(Mandatory = $true, Position = 1)]
+        [ValidateNotNullorEmpty()]
+        [Alias('Dbs')]
+        [string]$Database
+    )
+    Begin {
+
+        ## Query definition
+        [string]$Query =
+        "
+            DECLARE @Date DATETIME = DATEADD(hh, -12, GETDATE());
+            SELECT
+                CMG_Update_Scan = (
+                    SELECT COUNT(DISTINCT ResourceID)
+                    FROM v_UpdateScanStatus
+                    WHERE LastScanTime > @Date
+                        AND LastScanPackageLocation LIKE '%cmg%'
+                    GROUP BY LastScanPackageLocation
+                )
+                , CMG_Clients     = (
+                    SELECT COUNT(DISTINCT Name)
+                    FROM v_CombinedDeviceResources
+                    WHERE CNIsOnInternet = 1
+                        AND CNIsOnline = 1
+                        AND CNAccessMP LIKE '%cmg%'
+                )
+                , MP_Clients      = (
+                    SELECT COUNT(DISTINCT Name)
+                    FROM v_CombinedDeviceResources
+                    WHERE CNIsOnInternet = 0
+                        AND CNIsOnline = 1
+                );
+        "
+    }
+    Process {
+        Try {
+
+            ## Run SQL query
+            [psobject]$Result = Invoke-Sqlcmd -Query $Query -Server $Server -Database $Database -ErrorAction 'Stop'
+        }
+        Catch {
+            Write-Error -Message "Query Error: `n $_.ErrorMessage"
+        }
+        Finally {
+
+            ## Output result
+            Write-Output -InputObject $Result
+        }
+    }
+    End {
+    }
+}
+#endregion
